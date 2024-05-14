956f2df23de14b50d828e3f11c7b1beeeb324270
WDelesposti <wallace.delesposti@gmail.com>
2024-05-13 22:24:18 -0300

    refactor: fonts and welcome screen

--- App.tsx
+++ App.tsx
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Welcome } from "./src/pages/Welcome";
 import * as SplashScreen from "expo-splash-screen";
-import {
-  useFonts,
-  Jost_400Regular,
-  Jost_600SemiBold,
-} from "@expo-google-fonts/jost";
+import { Jost_400Regular, Jost_600SemiBold } from "@expo-google-fonts/jost";
+import * as Font from "expo-font";
+import { View } from "react-native";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Jost_400Regular,
-    Jost_600SemiBold,
-  });
+  const [appIsReady, setAppIsReady] = useState(false);
 
-  if (!fontsLoaded) {
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({
+          Jost_400Regular,
+          Jost_600SemiBold,
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
     return null;
   }
 
-  SplashScreen.preventAutoHideAsync();
-
-  return <Welcome />;
+  return (
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <Welcome />
+    </View>
+  );
 }
