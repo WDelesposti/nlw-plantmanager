9d8abed272bcdb32ac911cf4c57d5adb6a213a0d
WDelesposti <wallace.delesposti@gmail.com>
2024-05-14 10:05:13 -0300

    feat: build UserIdentification page

---
+++ src/pages/UserIdentification.tsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import {
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+
+import { Button } from "../components/Button";
+
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+
+export function UserIdentification() {
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+  const [name, setName] = useState<string>();
+
+  function handleInputBlur() {
+    setIsFocused(false);
+  }
+
+  function handleInputFocus() {
+    setIsFocused(true);
+  }
+
+  function handleInputChange(value: string) {
+    setIsFilled(!!value);
+    setName(value);
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <View style={styles.content}>
+          <View style={styles.form}>
+            <View style={styles.header}>
+              <Text style={styles.emoji}>
+                {isFilled ? "😄" : "😃"}
+              </Text>
+              <Text style={styles.title}>
+                Como podemos {"\n"}
+                chamar você?
+              </Text>
+            </View>
+            <TextInput
+              style={[styles.input, isFocused || isFilled ? { borderColor: colors.green } : { borderColor: colors.gray }]}
+              placeholder="Digite seu nome"
+              onBlur={handleInputBlur}
+              onFocus={handleInputFocus}
+              onChangeText={handleInputChange}
+            />
+            <View style={styles.footer}>
+              <Button />
+            </View>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  content: {
+    flex: 1,
+    width: "100%",
+  },
+  title: {
+    fontSize: 24,
+    textAlign: "center",
+    lineHeight: 32,
+    marginTop: 20,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+  },
+  form: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 54,
+    alignItems: "center",
+  },
+  header: {
+    alignItems: "center",
+  },
+  emoji: {
+    fontSize: 44,
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderColor: colors.gray,
+    color: colors.heading,
+    width: "100%",
+    fontSize: 18,
+    marginTop: 50,
+    padding: 10,
+    textAlign: "center",
+  },
+  footer: {
+    marginTop: 40,
+    width: "100%",
+    paddingHorizontal: 20,
+  },
+});
