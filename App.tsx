b9a89dcb53ab8a3956d34d3dd299894c014f668c
WDelesposti <wallace.delesposti@gmail.com>
2024-05-13 21:51:28 -0300

    chore: install fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import { Welcome } from "./src/pages/Welcome";
+import * as SplashScreen from "expo-splash-screen";
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold,
+} from "@expo-google-fonts/jost";
 
 export default function App() {
-  return (
-    <Welcome />
-  );
-}
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+  });
 
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  SplashScreen.preventAutoHideAsync();
+
+  return <Welcome />;
+}
