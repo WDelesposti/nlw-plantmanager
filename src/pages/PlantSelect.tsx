1aa2739522c0f61892969548b26b0164cc6fbd6e
WDelesposti <wallace.delesposti@gmail.com>
2024-05-14 18:47:33 -0300

    refactor: use fake api in FlatList

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { FlatList, StyleSheet, Text, View } from "react-native";
@@ -9,28 +9,45 @@ import { Header } from "../components/Header";
 import colors from "../styles/colors";
 import fonts from "../styles/fonts";
 import { EnviromentButton } from "../components/EnviromentButton";
+import api from "../services/api";
+
+interface EnvironmentProps {
+  key: string;
+  title: string;
+}
 
 export function PlantSelect() {
+  const [enviroments, setEnviroments] = useState<EnvironmentProps[]>([]);
+  useEffect(() => {
+    async function fetchEnviroment() {
+      const { data } = await api.get("plants_environments");
+      setEnviroments([
+        {
+          key: "all",
+          title: "Todos",
+        },
+        ...data,
+      ]);
+    }
+    fetchEnviroment();
+  }, []);
   return (
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Header />
-          <Text style={styles.title}>Em qual ambiente</Text>
-          <Text style={styles.subtitle}>você quer colocar sua planta?</Text>
-        </View>
-        <View>
-          <FlatList
-            data={[1, 2, 3, 4, 5]}
-            renderItem={({ item }) => (
-              <EnviromentButton title="Cozinha" active/>
-              
-            )}
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            contentContainerStyle={styles.environmentList}
-          />
-        </View>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Header />
+        <Text style={styles.title}>Em qual ambiente</Text>
+        <Text style={styles.subtitle}>você quer colocar sua planta?</Text>
       </View>
+      <View>
+        <FlatList
+          data={enviroments}
+          renderItem={({ item }) => <EnviromentButton title={item.title} />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.environmentList}
+        />
+      </View>
+    </View>
   );
 }
 
