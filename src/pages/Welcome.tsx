4a596e26d35061aadce4def0d892312ad0103923
WDelesposti <wallace.delesposti@gmail.com>
2024-05-13 21:30:04 -0300

    refactor: remove button component and use dimensions

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,15 +1,8 @@
 import React from "react";
-import {
-  Text,
-  SafeAreaView,
-  StyleSheet,
-  Image,
-  TouchableOpacity,
-} from "react-native";
+import { Text, SafeAreaView, StyleSheet, Image, TouchableOpacity, Dimensions } from "react-native";
 
 import wateringImg from "../assets/watering.png";
 import colors from "../styles/colors";
-import { Button } from "../components/Button";
 
 export function Welcome() {
   return (
@@ -20,14 +13,21 @@ export function Welcome() {
         de forma fácil
       </Text>
 
-      <Image source={wateringImg} style={styles.image} />
+      <Image
+        source={wateringImg}
+        style={styles.image}
+        resizeMode="contain"
+      />
 
       <Text style={styles.subtitle}>
         Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
         sempre que precisar.
       </Text>
 
-      <Button title=">" />
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+      <Text style={styles.buttonText}>></Text>
+    </TouchableOpacity>
+
     </SafeAreaView>
   );
 }
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "space-between",
+    justifyContent: "space-around",
   },
   title: {
     fontSize: 32,
@@ -52,7 +52,19 @@ const styles = StyleSheet.create({
     color: colors.heading,
   },
   image: {
-    width: 292,
-    height: 284,
+    height: Dimensions.get("window").width * 0.7,
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 24,
   },
 });
