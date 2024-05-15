f6d78b2fe0819b67f5925cbf93b6516890d96ff1
WDelesposti <wallace.delesposti@gmail.com>
2024-05-15 19:46:26 -0300

    refactor: PlantProps interface

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -11,23 +11,11 @@ import { PlantCardPrimary } from "../components/PlantCardPrimary";
 import { Load } from "../components/Load";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { PlantProps } from "../libs/storage";
 
 interface EnvironmentProps {
   key: string;
   title: string;
-}
-
-interface PlantProps {
-  id: string;
-  name: string;
-  about: string;
-  water_tips: string;
-  photo: string;
-  environments: string[];
-  frequency: {
-    times: number;
-    repeat_every: string;
-  };
 }
 
 export type PlantSaveParamList = {
