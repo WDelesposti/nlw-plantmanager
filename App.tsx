fc7da385ef7c3044d1da8d785b45a1ee9c2d16d0
WDelesposti <wallace.delesposti@gmail.com>
2024-05-16 22:16:14 -0300

    feat: add notification

--- App.tsx
+++ App.tsx
@@ -3,11 +3,37 @@ import * as SplashScreen from "expo-splash-screen";
 import { Jost_400Regular, Jost_600SemiBold } from "@expo-google-fonts/jost";
 import * as Font from "expo-font";
 import { View } from "react-native";
+import * as Notifications from "expo-notifications";
 
 import Routes from "./src/routes/";
+import { PlantProps } from "./src/libs/storage";
 
 export default function App() {
   const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    // const subscription = Notifications.addNotificationReceivedListener(
+    //   async (notification) => {
+    //     const data = notification.request.content.data.plant as PlantProps;
+    //     console.log(data);
+    //   }
+    // );
+
+    // return () => subscription.remove();
+
+    // async function notifications() {
+    //   const data = await Notifications.getAllScheduledNotificationsAsync();
+    //   console.log("NOTIFICAÇÕES AGENDADAS: ", data);
+    // }
+
+    // notifications();
+    // async function notifications() {
+    //   await Notifications.cancelAllScheduledNotificationsAsync();
+    //   const data = await Notifications.getAllScheduledNotificationsAsync();
+    //   console.log("NOTIFICAÇÕES AGENDADAS: ", data);
+    // }
+    // notifications();
+  }, []);
 
   useEffect(() => {
     async function prepare() {
